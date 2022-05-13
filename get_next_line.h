e1ec19ddd6388db38f6f9c08ee2271f493226d66
aminoru- <aminoru-@student.42sp.org.br>
2022-05-13 23:11:24 +0200

    update

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 17:00:39 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/05/12 19:23:29 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/05/13 22:51:33 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ char	*get_next_line(int fd);
 
 // utils
 char	*ft_strchr(const char *s, int c);
-void	ft_strlcpy_2(char *dest, const char *src, size_t size);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 18:38:36 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/05/11 19:58:58 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/05/13 22:51:39 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ char	*get_next_line(int fd);
 
 // utils
 char	*ft_strchr(const char *s, int c);
-void	ft_strlcpy_2(char *dest, const char *src, size_t size);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
